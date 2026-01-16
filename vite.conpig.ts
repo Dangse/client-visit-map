e281a1f1513c91467bf08e2959aeca6aa9de93c8
dangse <49353260+Dangse@users.noreply.github.com>
2026-01-16 18:08:54 +0900

    구글API 연결
    구글API 연결

--- vite.conpig.ts
+++ vite.conpig.ts
@@ -2,11 +2,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // base를 './'로 설정하면 저장소 이름에 상관없이 상대 경로로 리소스를 찾습니다.
   base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false
+  },
+  // GitHub Secrets에 저장된 API_KEY를 브라우저 환경에서 인식할 수 있게 변환합니다.
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
