3be35063fae036bf8345a8b0c6af84e41133341a
dangse <49353260+Dangse@users.noreply.github.com>
2026-01-17 13:22:56 +0900

    빌드환경 구성
    빌드환경 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss({config: path.resolve(__dirname, 'tailwind.config.js')}),
+        autoprefixer()
+      ],
+    },
+  },
   // GitHub Pages의 리포지토리 이름과 일치시켜야 합니다.
   // 예: https://user.github.io/client-visit-map/
   base: '/client-visit-map/',
